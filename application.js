14fa115ce306b43aef1b373afa40390bd7297aab
JT Grauke <jtgrauke@gmail.com>
2013-08-10 13:50:24 -0700

    Minor style changes

--- application.js
+++ application.js
@@ -1,4 +1,3 @@
-
 // Take the user input upon click, and add it as a to-do item in a list
 function clickItem() {
     // Grab the value of the user input
@@ -9,8 +8,7 @@ function clickItem() {
     // Once we know there's something to add, we can run the rest of the function
     } else {
         // Write the html for the to list item, and throw in the users input as the value of the to-do item
-        itemStatus = '<i class="icon-ok"></i>';
-        todoItem = '<li class="to-do-wrapper"><input type="text" class="to-do item" value="' + userInput + '" readonly><span id="userAction" class="submit complete-item">' + itemStatus + '</span></li>';
+        todoItem = '<li class="to-do-wrapper"><input type="text" class="to-do item" value="' + userInput + '" readonly><span id="userAction" class="submit complete-item"><i class="icon-remove"></i></span></li>';
         // Now place it on the page in the right spot
         $(todoItem).appendTo('.to-do-items-wrapper').slideDown(250);
         // Reset the value of the users input to '' so they don't accidentally add the same item twice
@@ -33,18 +31,19 @@ function enterItem() {
 // Function to change the styling for when an item is marked complete
 function completeItem() {
     // Find the check mark button, and when it's clicked...
-    $(this).find('#userAction').click(function(){
-        // Add a completed class
-        $(this).parent().addClass('completed');
-        // And display the delete button to remove them
+    $(this).find('#userAction').parent().toggleClass('completed');
+    // If the completed class is found, show the option to delete them, if not, hide the delete trigger
+    if ($('.to-do-wrapper').hasClass('completed')) {
         $('.delete-completed').slideDown();
-    });
+    } else {
+        $('.delete-completed').slideUp();
+    }
 }
 
 // Delete the complete items
 function deleteCompleted() {
     // Find the ones that have a completed class and slide them up
-    $(this).closest('.wrapper').find('.to-do-items-wrapper').children('.completed').slideUp();
+    $(this).closest('.wrapper').find('.to-do-items-wrapper').children('.completed').slideUp(function() { $(this).remove(); });
     // Now hide the delete trigger since there's nothing more to delete
     $('.delete-completed').slideUp();
 }
@@ -55,7 +54,7 @@ $(document).ready(function() {
     $('#userInput').keydown(enterItem);
     $('.add').click(clickItem);
     $('.delete-completed').click(deleteCompleted);
-    $('.to-do-items-wrapper').on('mouseenter', '.to-do-wrapper', completeItem);
+    $('.to-do-items-wrapper').on('click', '.to-do-wrapper', completeItem);
 
 }); // END OF DOCUMENT
 
