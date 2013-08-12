1937f265db5a2780f1fab21077904e4d78baaad3
JT Grauke <jtgrauke@gmail.com>
2013-08-12 16:02:31 -0700

    Adding variables for styles

--- application.js
+++ application.js
@@ -1,14 +1,20 @@
+
+// Setting up the right classes to be used througout the UI
+var selectedClass = 'icon-circle';
+var unselectedClass = 'icon-circle-blank';
+
 // Take the user input upon click, and add it as a to-do item in a list
 function clickItem() {
     // Grab the value of the user input
     userInput = document.getElementById("userInput").value;
+
     // Make sure there is something to actually add so we don't get a blank item
     if (userInput === '') {
         return;
     // Once we know there's something to add, we can run the rest of the function
     } else {
         // Write the html for the to list item, and throw in the users input as the value of the to-do item
-        todoItem = '<li class="to-do-wrapper"><input type="text" class="to-do item" value="' + userInput + '" readonly><span id="userAction" class="submit complete-item"><i class="icon-circle-blank"></i></span></li>';
+        todoItem = '<li class="to-do-wrapper"><input type="text" class="to-do item" value="' + userInput + '" readonly><span id="userAction" class="submit complete-item"><i class="'+ unselectedClass +'"></i></span></li>';
         // Now place it on the page in the right spot
         $(todoItem).appendTo('.to-do-items-wrapper').slideDown(250);
         // Reset the value of the users input to '' so they don't accidentally add the same item twice
@@ -31,9 +37,6 @@ function enterItem() {
 // Function to change the styling for when an item is marked complete
 function completeItem() {
     $(this).find('#userAction').parent().toggleClass('completed');
-
-    var selectedClass = 'icon-circle';
-    var unselectedClass = 'icon-circle-blank';
 
     // If the completed class is found, show the icon-ok, as opposed to the icon-remove
     if ($(this).closest('.to-do-wrapper').hasClass('completed')) {
@@ -63,12 +66,8 @@ $(document).ready(function() {
     // Event Handlers
     $('#userInput').keydown(enterItem);
     $('.add').click(clickItem);
-    $('.delete-completed').on('mouseenter', function(){
-        $('.completed').addClass('about-to-delete');
-    });
-    $('.delete-completed').on('mouseleave', function(){
-        $('.completed').removeClass('about-to-delete');
-    });
+    $('.delete-completed').on('mouseenter', function(){$('.completed').addClass('about-to-delete');});
+    $('.delete-completed').on('mouseleave', function(){$('.completed').removeClass('about-to-delete');});
     $('.delete-completed').click(deleteCompleted);
     $('.to-do-items-wrapper').on('click', '.to-do-wrapper', completeItem);
 
